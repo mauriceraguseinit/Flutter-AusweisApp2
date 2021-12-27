9e1d535963476a85c122627d4dbb9a1ec41d54a0
Maurice Raguse <maurice.raguse@init.de>
2021-12-27 10:52:49 +0100

    add framework and example

---
+++ Example/Pods/Target Support Files/Flutter-AusweisApp2/Flutter-AusweisApp2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Flutter_AusweisApp2 : NSObject
+@end
+@implementation PodsDummy_Flutter_AusweisApp2
+@end
---
+++ Example/Pods/Target Support Files/Pods-Flutter-AusweisApp2_Example/Pods-Flutter-AusweisApp2_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flutter_AusweisApp2_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Flutter_AusweisApp2_Example
+@end
